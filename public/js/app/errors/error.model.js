f8be8b981628ed07738c8aa6b28db7dbc23607c2
Farid Hajnal <faridhajnal14@hotmail.com>
2017-02-02 01:07:08 -0600

    Version normal ALV

--- public/js/app/errors/error.model.js
+++
@@ -1,8 +0,0 @@
-export var Error = (function () {
-    function Error(title, content) {
-        this.title = title;
-        this.content = content;
-    }
-    return Error;
-}());
-//# sourceMappingURL=error.model.js.map
